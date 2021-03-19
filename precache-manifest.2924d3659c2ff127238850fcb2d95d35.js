3e5114a27ade812293df3f3c40ebbe07ef8be51b
gazzati <gazzaevtimur@gmail.com>
2021-03-19 23:04:10 +0300

    Updates

---
+++ precache-manifest.2924d3659c2ff127238850fcb2d95d35.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963e4752f602174f950c54123c9b72f2",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "78c841e2b0e0ec39f2e7",
+    "url": "/social-network/static/css/main.209a9f46.chunk.css"
+  },
+  {
+    "revision": "d13fec7290d81528d0db",
+    "url": "/social-network/static/js/2.4b1da3d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.4b1da3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c841e2b0e0ec39f2e7",
+    "url": "/social-network/static/js/main.30a4f255.chunk.js"
+  },
+  {
+    "revision": "6fd508d7faecb3b86bbc",
+    "url": "/social-network/static/js/runtime-main.8a9b8e37.js"
+  },
+  {
+    "revision": "cdf0ae9bc46bd9cd048c7e92a33bfc5c",
+    "url": "/social-network/static/media/facebook.cdf0ae9b.svg"
+  },
+  {
+    "revision": "dbf7b3f23bec85dbd50d961d05788974",
+    "url": "/social-network/static/media/github.dbf7b3f2.svg"
+  },
+  {
+    "revision": "ed1683c17ff82f0e31fafc179df561f3",
+    "url": "/social-network/static/media/instagram.ed1683c1.svg"
+  },
+  {
+    "revision": "e980087a4fa9362485562748c2257a8f",
+    "url": "/social-network/static/media/left-arrow.e980087a.svg"
+  },
+  {
+    "revision": "38f3d53cb559964b250b160c53a1e4c7",
+    "url": "/social-network/static/media/leftArrowIcon.38f3d53c.svg"
+  },
+  {
+    "revision": "d341c6cff5a25fb50b95bd5fa12af003",
+    "url": "/social-network/static/media/preloader.d341c6cf.svg"
+  },
+  {
+    "revision": "2b8638d18c6a7648a3cf3a90e4ce228f",
+    "url": "/social-network/static/media/preloaderMini.2b8638d1.svg"
+  },
+  {
+    "revision": "e45858263764bac722b780add2c1e029",
+    "url": "/social-network/static/media/removePost.e4585826.svg"
+  },
+  {
+    "revision": "70de6af45810602ff4946f0d33452fac",
+    "url": "/social-network/static/media/rightArrowIcon.70de6af4.svg"
+  },
+  {
+    "revision": "b2de10d4a3d1ac6e049b991761644025",
+    "url": "/social-network/static/media/twitter.b2de10d4.svg"
+  },
+  {
+    "revision": "fbe89cad4e8786bbc6221cbdb2b6fa8a",
+    "url": "/social-network/static/media/user.fbe89cad.png"
+  },
+  {
+    "revision": "786fa8e640960c1cc2d31b671ef96422",
+    "url": "/social-network/static/media/view.786fa8e6.svg"
+  },
+  {
+    "revision": "f5207225fc781dacd9a62386dca56d9d",
+    "url": "/social-network/static/media/vk.f5207225.svg"
+  },
+  {
+    "revision": "0adb6b6874ff3ec9976ae6bd4bc20418",
+    "url": "/social-network/static/media/youtube.0adb6b68.svg"
+  }
+]);
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as serviceWorker from './serviceWorker'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MainApp from './App'
-
-ReactDOM.render(<MainApp/>, document.getElementById('root'))
-
-serviceWorker.unregister()
